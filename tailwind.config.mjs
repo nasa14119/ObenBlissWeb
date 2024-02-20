8f1299c767259e2e68efea6d1b39d68d3aa4ff84
nasa14119 <nico.amaya0205@gmail.com>
2024-02-19 19:05:55 -0600

    feat(Hero): adding Hero to home page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,14 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		colors:{
+			primary : "#f5bb00ff",
+			secundary: "#264653ff",
+			accent: "#FAC54F",
+			contrast :"#d76a03ff",
+			extra: "#bf3100ff",
+			violet: "#702632"
+		}
 	},
 	plugins: [],
 }
